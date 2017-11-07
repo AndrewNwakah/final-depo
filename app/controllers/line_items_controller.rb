258690bed429c8d5264c115ffc5d5806a7a252f7
AndrewNwakah <andrewnwakah@gmail.com>
2017-11-07 17:04:22 +0000

    New Lab

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,10 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+  
 include CurrentCart
 	before_action :set_cart, only: [:create]
+	
+	
 	
   # GET /line_items
   # GET /line_items.json
@@ -28,8 +30,8 @@ include CurrentCart
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-	@line_item = @cart.line_items.build(product: product)
-
+	# @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
@@ -72,7 +74,12 @@ include CurrentCart
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
+     #def line_item_params
+     # params.require(:line_item).permit(:product_id, :cart_id)
+   # end
+    
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+    #  params.require(:line_item).permit(:product_id, :cart_id)
+    params.require(:line_item).permit(:product_id)
     end
 end
